772007ba91879e40dd55ba5bdd8bb9fc4d18767f
rameshn Ch <rameshn3@gmail.com>
2023-08-20 18:27:35 -0700

    added logger to the forgotpasswordtest file

--- src/test/java/com/qa/opencart/testcase/ForgotPasswordPageTest.java
+++ src/test/java/com/qa/opencart/testcase/ForgotPasswordPageTest.java
@@ -1,7 +1,10 @@
 package com.qa.opencart.testcase;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.qa.opencart.factory.TestBase;
 
 public class ForgotPasswordPageTest extends TestBase{
-
+	private Logger log=LogManager.getLogger(ForgotPasswordPageTest.class.getName());
 }
